17f13507b74a1b47a5ff660b5d31f54571c54984
Berke Ates <wppgamer@users.noreply.github.com>
2016-12-06 20:23:20 +0100

    Added Loading and Error Pics and some design changes

--- Magic2Brain/app/src/main/java/m2b/magic2brain/com/QueryActivity.java
+++ Magic2Brain/app/src/main/java/m2b/magic2brain/com/QueryActivity.java
@@ -2,6 +2,7 @@ package m2b.magic2brain.com;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,25 +18,52 @@ public class QueryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query);
         final EditText inputtxt = (EditText) findViewById(R.id.cardname_input);
-        inputtxt.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                inputtxt.setText("");
+        inputtxt.setOnKeyListener(new View.OnKeyListener(){
+            public boolean onKey(View v, int keyCode, KeyEvent event){
+                if (event.getAction() == KeyEvent.ACTION_DOWN){
+                    switch (keyCode) {
+                        case KeyEvent.KEYCODE_DPAD_CENTER:
+                        case KeyEvent.KEYCODE_ENTER:
+                            checkAnswer(inputtxt.getText().toString());
+                            return true;
+                        default:
+                            break;
+                    }
+                }
+                return false;
             }
         });
 
         Button answer = (Button) findViewById(R.id.answer_button);
         answer.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                showPic(410018);
+                checkAnswer(inputtxt.getText().toString());
             }
         });
+
 
         showPic(410017);
     }
 
     public void showPic(int MultiID){
         ImageView b = (ImageView) findViewById(R.id.imageViewQuiz);
-        Picasso.with(this).load("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + MultiID + "&type=card").into(b);
+        Picasso.with(this)
+                .load("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + MultiID + "&type=card")
+                .placeholder(R.drawable.white_loader)
+                .error(R.drawable.white_notfound)
+                .into(b);
     }
 
+    public void showPic(String MultiID){
+        ImageView b = (ImageView) findViewById(R.id.imageViewQuiz);
+        Picasso.with(this)
+                .load("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + MultiID + "&type=card")
+                .placeholder(R.drawable.white_loader)
+                .error(R.drawable.white_notfound)
+                .into(b);
+    }
+
+    public void checkAnswer(String txt){
+        showPic(txt);
+    }
 }
