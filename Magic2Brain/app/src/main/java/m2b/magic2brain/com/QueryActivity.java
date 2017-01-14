4d6c32d96ba3e4a571bc4525473f3f73074e8170
Berke Ates <wppgamer@users.noreply.github.com>
2017-01-14 15:51:24 +0100

    dgf

--- Magic2Brain/app/src/main/java/m2b/magic2brain/com/QueryActivity.java
+++ Magic2Brain/app/src/main/java/m2b/magic2brain/com/QueryActivity.java
@@ -70,7 +70,7 @@ public class QueryActivity extends AppCompatActivity {
         if(deckName == null){deckName="DEFAULT";}
         setTitle(deckName);
         if(!loadRecent()){recentlyLearned = new ArrayList<>();}
-        if(!recentlyLearned.contains(code)){recentlyLearned.add(code);} //TODO: CODE DOESNT WORK. DECKNAME DOES. WHY THE FUCK?
+        if(!recentlyLearned.contains(code)){recentlyLearned.add(code);}
         if(recentlyLearned.size() == 2){recentlyLearned.remove(0);}
         saveRecent();
         set = qur.getSet();
