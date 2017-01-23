dd60b6c90c321476532410c22aaf4c0882190778
Berke Ates <wppgamer@users.noreply.github.com>
2017-01-23 19:34:07 +0100

    added comments

--- Magic2Brain/app/src/main/java/m2b/magic2brain/com/MainActivity.java
+++ Magic2Brain/app/src/main/java/m2b/magic2brain/com/MainActivity.java
@@ -28,40 +28,44 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import m2b.magic2brain.com.magic2brain.R;
 
+/*
+This is where our app starts. It has a menu, shows a random card and loads some needed data.
+*/
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    public static Card[] cardarray;
-    private ImageView imgv;
+    public static Card[] cardarray; // We will store all cards in this array. This is important for the SearchActivity. It makes the whole process alot faster.
+    private ImageView imgv; // We will show the random card with this ImageView
 
-    protected void onCreate(Bundle savedInstanceState) {
-        if (!checkCardArray()) {
-            saveCardArray();
+    protected void onCreate(Bundle savedInstanceState) { // This is the very first function the app will execute at the start of the app.
+        if (!checkCardArray()) { // We try to load our card-list.
+            saveCardArray(); // If we don't find the card-list, we have to generate it and save it.
         }
-        hideStatusBar();
-        setTheme(R.style.AppTheme_NoActionBar);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        hideStatusBar(); // Does what is says. It hides the Statusbar
+        setTheme(R.style.AppTheme_NoActionBar);  // We want to hide the ActionBar too
+        super.onCreate(savedInstanceState); // This does some intern stuff. We don't need to worry about that. It's just needed.
+        setContentView(R.layout.activity_main); // This adds an View to our Activity. We defined at "/res/layout/activity_main.xml" how our activity should look like.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); // With this we get the Toolbar of the View.
+        setSupportActionBar(toolbar); // We add the Toolbar as a SupportActionBar. If we click something on the Toolbar it will call onNavigationItemSelected();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab); // We get the "new random card"-button
         fab.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                setListener(setRandomCard());
+                setListener(setRandomCard()); // If we click the button it should generate a new random card.
             }
         });
-
+        // The following lines generate the drawer (Sidemenu)
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        Favorites.init();
 
-        // Restore preferences
+        Favorites.init(); // Initate Favorites
+
+        // The following code loads the Favorites from the memory
         SharedPreferences appSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         Gson gson = new Gson();
         String json = appSharedPrefs.getString("favobj", null);
@@ -72,18 +76,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (favs != null) {
             Favorites.favorites_mvid = favs;
         }
-        buildNewsFeed();
+        buildNewsFeed(); // This builds the frontpage
     }
 
-    private void hideStatusBar() {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    private void hideStatusBar() { // This method simply removes the Statusbar
+        requestWindowFeature(Window.FEATURE_NO_TITLE); // remove the title
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // set it to fullscreen
     }
 
     protected void onPause() {
         super.onPause();
-        // We need an Editor object to make preference changes.
-        // All objects are from android.context.Context
+        // If the user leaves our app in any way, we save all his favorites.
         SharedPreferences appSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor prefsEditor = appSharedPrefs.edit();
         Gson gson = new Gson();
@@ -92,7 +95,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         prefsEditor.commit();
     }
 
-    public void onBackPressed() {
+    public void onBackPressed() { // Closes the Drawer if we press the Back-Button on the phone
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
@@ -105,19 +108,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
         if (id == R.id.nav_search) {
-            // Handle the camera action
+            // Starts the SearchActivitiy if we click on Search
             Intent intent = new Intent(this, SearchActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_favorite) {
+            // Starts the FavoritesActivity if we click ond Favorites
             Intent intent = new Intent(this, FavoritesActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_browser) {
+            // Starts the BrowserActivity if we click on the Cardbrowser
             Intent intent = new Intent(this, BrowserActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_quick_learn) {
+            // Starts the QueryActivity and passes the newest set (Kaladesh), if we click Quick learn
             Deck d = new Deck();
             Card[] c = DeckAssetLoader.getDeck("KLD.json",this);
             d.setSet(c);
@@ -128,10 +134,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             startActivity(i);
 
         } else if (id == R.id.nav_history) {
+            // Starts the LastSeenActivity if we click recently learned
             Intent intent = new Intent(this, LastSeenActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_share) {
+            // Starts an Activity, which opens all possibilities to send the link to our app.
             Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
             sendIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.play_store_link));
@@ -139,23 +147,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.send_to)));
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout); // We get the drawer
+        drawer.closeDrawer(GravityCompat.START); // Th drawer should start closed
         return true;
     }
 
-    public void buildNewsFeed() {
-        int scrWidth = getWindowManager().getDefaultDisplay().getWidth();
-        int scrHeight = getWindowManager().getDefaultDisplay().getHeight();
+    public void buildNewsFeed() { // This Function generates a text and a Imageview on the screen
+        int scrWidth = getWindowManager().getDefaultDisplay().getWidth(); // Get the width of the screen
+        int scrHeight = getWindowManager().getDefaultDisplay().getHeight(); // Get the Height of the screen
         RelativeLayout lyt = (RelativeLayout) findViewById(R.id.main_absolute); // Get the View of the XML
-        RelativeLayout.LayoutParams params;
+        RelativeLayout.LayoutParams params; // The parameters we want to add our TextView and ImageView
 
         TextView score = new TextView(this); // Create new Textview
         score.setTextColor(Color.BLACK);
         score.setGravity(Gravity.CENTER);
         score.setText("Random Card");
         score.setTextSize(26);
-        params = new RelativeLayout.LayoutParams(scrWidth, (int) (0.1 * scrHeight));
+        params = new RelativeLayout.LayoutParams(scrWidth /*Width*/, (int) (0.1 * scrHeight)/*Height*/);
         params.leftMargin = 0; // X-Position
         params.topMargin = (int) (0.1 * scrHeight); // Y-Position
         lyt.addView(score, params); // add it to the View
@@ -165,10 +173,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         params.leftMargin = 0; // X-Position
         params.topMargin = (int) (0.2 * scrHeight); // Y-Position
         lyt.addView(imgv, params); // add it to the View
-        setListener(setRandomCard());
+        setListener(setRandomCard()); // Opens a random card
     }
 
-    public void setListener(int MultiID2) {
+    public void setListener(int MultiID2) { // This function sets an listener to the image, so if we click it, it shows a big image of the card.
         final int MultiID = MultiID2;
         imgv.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -179,21 +187,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         });
     }
 
-    public int setRandomCard() {
+    public int setRandomCard() { // This gets a random card from the card-array and loads the image into the ImageView
         int MultiID = 1;
         Card c = cardarray[(int) (Math.random() * cardarray.length)];
         if (c != null) {
             MultiID = c.getMultiverseid();
         }
         Picasso.with(this)
-                .load("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + MultiID + "&type=card")
-                .placeholder(R.drawable.loading_image)
-                .error(R.drawable.image_not_found)
-                .into(imgv);
+                .load(getString(R.string.image_link_1) + MultiID +  getString(R.string.image_link_2)) // This tries to load an image from a link.
+                .placeholder(R.drawable.loading_image) // We want to show a image while its loading. We load our image from the "/res/drawable" folder
+                .error(R.drawable.image_not_found)  // If it fails to load image we show an error-image.
+                .into(imgv);  // This places the image into our ImageView.
         return MultiID;
     }
 
-    public boolean checkCardArray() {
+    public boolean checkCardArray() { // This method checks if there is a saved version of our card array. If that's the case it tries to load it into the card-array.
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         Gson gson = new Gson();
         String json = sharedPrefs.getString("cardArray", null);
@@ -205,7 +213,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
-    public void saveCardArray() {
+    public void saveCardArray() { // This function saves the card-array into the memory.
         Card[] c = buildCardArray();
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = sharedPrefs.edit();
@@ -216,7 +224,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         cardarray = c;
     }
 
-    private Card[] buildCardArray() {
+    private Card[] buildCardArray() { // This method generates the card-array by opening all sets and loading the cards from it into out array.
         ArrayList<Card> list = new ArrayList<>();
         Deck[] deckarray = DeckAssetLoader.getDeckList(this);
         for (int i = 0; i < deckarray.length; i++) {
--- Magic2Brain/app/src/main/java/m2b/magic2brain/com/SearchActivity.java
+++ Magic2Brain/app/src/main/java/m2b/magic2brain/com/SearchActivity.java
@@ -10,21 +10,25 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import m2b.magic2brain.com.magic2brain.R;
 
+/*
+This Activity asks the user if he wants to search for cards or set and opens a SearchHandlerActivity if the answer.
+*/
+
 public class SearchActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
-        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        final Context context = this;
-        setTitle(getString(R.string.SearchActivity_title));
-        Button cards = (Button) findViewById(R.id.c_search_cards);
-        Button decks = (Button) findViewById(R.id.c_search_decks);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left); // This adds an fancy slide animation, when this activity starts.
+        super.onCreate(savedInstanceState); // This does some intern stuff. We don't need to worry about that. It's just needed.
+        setContentView(R.layout.activity_search); // This adds an View to our Activity. We defined at "/res/layout/activity_search.xml" how our activity should look like.
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // With this line we add an "back"-Button to the Toolbar. If we press it it calls onOptionsItemSelected();
+        final Context context = this; // This doesn't actually do anything, but it's needed if we want to refer to "this" from an inner class.
+        setTitle(getString(R.string.SearchActivity_title)); // We set a title to our View. We defined the title in "/res/values/strings.xml". We just load it from there.
+        Button cards = (Button) findViewById(R.id.c_search_cards); // We get the cards-button
+        Button decks = (Button) findViewById(R.id.c_search_decks); // We get the sets-button
 
         cards.setOnClickListener(new OnClickListener() {
-            @Override
             public void onClick(View view) {
+                // If the user presses the cards-button, we start SearchHandlerActivity and pass to it, that the user pressed "cards"
                 Intent intent = new Intent(context, SearchHandlerActivity.class);
                 intent.putExtra("cardsearch", true);
                 startActivity(intent);
@@ -32,8 +36,8 @@ public class SearchActivity extends AppCompatActivity {
         });
 
         decks.setOnClickListener(new OnClickListener() {
-            @Override
             public void onClick(View view) {
+                // If the user presses the sets-button, we start SearchHandlerActivity and pass to it, that the user pressed "sets"
                 Intent intent = new Intent(context, SearchHandlerActivity.class);
                 intent.putExtra("cardsearch", false);
                 startActivity(intent);
@@ -46,13 +50,12 @@ public class SearchActivity extends AppCompatActivity {
             // Respond to the action bar's Up/Home button
             case android.R.id.home:
                 onBackPressed();
-                return true;
         }
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
     public void onBackPressed() {
-        finish();
-        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+        finish(); // This closes our Activity
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right); // We want to close it with an fancy animation.
     }
 }
